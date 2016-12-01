ebb47882f58427d41ca8d8b8446e0d6a72cfa9af
tim <tim@iMac-tim.local>
2016-12-01 16:15:48 +0300

    can compile and upload build

--- deploy_yandex.sh
+++ deploy_yandex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-UPLOAD_URL=$(curl -H "Authorization: OAuth $YANDEX_TOKEN" "https://cloud-api.yandex.net/v1/disk/resources/upload?fields=href&overwrite=true&path=%2Felixir_releases%2Fstudio_ui_admin" | sed -e 's/^.*"href"[ ]*:[ ]*"//' -e 's/".*//') &&
+UPLOAD_URL=$(curl -H "Authorization: OAuth $YANDEX_TOKEN" "https://cloud-api.yandex.net/v1/disk/resources/upload?fields=href&overwrite=true&path=%2Fdesktop_builds%2Fstudio_ui_admin.zip" | sed -e 's/^.*"href"[ ]*:[ ]*"//' -e 's/".*//') &&
 curl $UPLOAD_URL --upload-file $1

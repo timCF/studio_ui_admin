2a329edb9d41fe6a602af60a6da46ff3a6c79557
tim <tim@ipa-win-.lan>
2017-01-13 14:49:28 +0300

    fix desktop app https url

--- electron/main.js
+++ electron/main.js
@@ -9,7 +9,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(`http://193.70.100.32:7772`)
+  win.loadURL(`https://crm.lemooorstudio.ru:7772`)
 
   // Open the DevTools.
   // win.webContents.openDevTools()
